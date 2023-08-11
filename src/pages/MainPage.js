d59cbd29e649cadc2f5ef2d432a1d3c8e9f43280
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-08-11 11:00:27 +0900

    black bubble

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import mainBack from '../assets/images/main-back.jpg'
 import earthOnHand from '../assets/images/earth-on-hand.png'
-import coTwo1 from '../assets/images/co2-1.png'
-import coTwo2 from '../assets/images/co2-2.png'
+import coTwoBubble from '../assets/images/co2-bubble.png'
 import Footer from '../components/Footer'
 function MainPage() {
   return (
@@ -18,13 +17,13 @@ function MainPage() {
             </div>
             <img src={earthOnHand} alt="Earth on Hand" className='earth-on-hand'/>
             <div className='bubbles'>
-                <img src={coTwo1} alt="CO2-1" className='co2'/>
-                <img src={coTwo1} alt="CO2-1" className='co2'/>
-                <img src={coTwo1} alt="CO2-1" className='co2'/>
-                <img src={coTwo1} alt="CO2-1" className='co2'/>
-                <img src={coTwo1} alt="CO2-1" className='co2'/>
-                <img src={coTwo2} alt="CO2-2" className='co2'/>
-                <img src={coTwo2} alt="CO2-2" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
+                <img src={coTwoBubble} alt="CO2-bubble" className='co2'/>
             </div>
         </div>
         <Footer/>
