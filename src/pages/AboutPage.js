0dce4318b7a0472a296296ddb664bd19c8819753
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-08-17 13:27:38 +0900

    example screenshot

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Header from '../components/Header'
 import aboutPageImage from '../assets/images/aboutpage-image.png'
 import aboutPageImageTwo from '../assets/images/aboutpage-image-2.png'
+import exampleScreenshot from '../assets/images/example-screenshot.png'
 import Footer from '../components/Footer'
 import { ReactComponent as BusinessIcon1 } from '../assets/svg/business-1.svg'
 import { ReactComponent as BusinessIcon2 } from '../assets/svg/business-2.svg'
@@ -26,21 +27,26 @@ function AboutPage({ sideBarFlag, setSideBarFlag }) {
       </div>
       <div className={itemNumber === 0 ? 'description' : 'hidden'}>
         <div className='text'>
-          <span>VeriCO2 is a cutting-edge proprietary technology that specialises in the efficient quantification of scope 3 emissions.
+          <span className='title'>Overview</span>
+          <span><b>VeriCO₂</b> is a cutting-edge proprietary technology that specialises in the efficient quantification of scope 3 emissions.
             By automating the retrieval of emission factors and consolidating scope 1 and 2 emissions from suppliers, it provides
             a comprehensive and transparent view of a company's entire scope 3 emissions profile.
-            Along with detailed data, VeriCO2 offers aesthetically pleasing visualisations that enable users to grasp the information easily.</span>
-
-          <span>In its ambitious Phase 2 rollout, VeriCO2 is set to become an essential tool in the fight against greenwashing.
+            Along with detailed data, <b>VeriCO₂</b> offers aesthetically pleasing visualisations that enable users to grasp the information easily.</span>
+            
+          <div className='image-card'>
+            <img src={exampleScreenshot} alt="Example Screenshot" className='example-screenshot' />
+          </div>
+          <span className='title'>Phase 2</span>
+          <span>In its ambitious Phase 2 rollout, <b>VeriCO₂</b> is set to become an essential tool in the fight against greenwashing.
             It will utilise advanced natural language processing algorithms to critically evaluate companies' annual disclosures
             against the stringent requirements of IFRS S2 standards, focused on climate-related risks and opportunities.
             This rigorous analysis will highlight any deficiencies in disclosure, providing clear guidance on what is missing and
             what must be amended to achieve compliance.</span>
 
-          <span>More than just an analytical tool, VeriCO2 aims to encourage corporate responsibility and environmental integrity.
-            By identifying companies with robust carbon disclosure alignment and commendable emissions performance, VeriCO2 sets the
+          <span>More than just an analytical tool, <b>VeriCO₂</b> aims to encourage corporate responsibility and environmental integrity.
+            By identifying companies with robust carbon disclosure alignment and commendable emissions performance, <b>VeriCO₂</b> sets the
             stage for the fair distribution of environmental credits to those buyers that demonstrate a genuine commitment to sustainability.
-            With VeriCO2, stakeholders can make more informed decisions, fostering a more transparent and accountable business environment.
+            With <b>VeriCO₂</b>, stakeholders can make more informed decisions, fostering a more transparent and accountable business environment.
           </span>
         </div>
       </div>
